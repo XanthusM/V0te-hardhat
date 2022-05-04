43a13aa38e0aa0f8d2f44149725ba77b29909175
XanthusM <91717625+XanthusM@users.noreply.github.com>
2022-05-04 16:51:30 +0700

    Add files via upload

--- contracts/Vote.sol
+++ contracts/Vote.sol
@@ -7,8 +7,9 @@ contract Vote {
     mapping (address => uint) public balance;
     mapping(address => bool) private Voted; // To check voters cast their votes before
     mapping (uint => Candidate) public candidates;
-    uint public prizeAmount;
     uint private count;
+    address payable prizeAmount;
+    uint public amount;
 
     modifier onlyOwner() {
         require(msg.sender == owner);
@@ -16,10 +17,6 @@ contract Vote {
     }
 
     event UpVote(string Candidate, uint upVoate);
-
-    constructor(uint _prizeAmount){
-        prizeAmount= _prizeAmount;
-    }
 
     struct Candidate {
         uint id;
@@ -34,11 +31,12 @@ contract Vote {
     }
 
     function upV0te(uint candidateId) public payable {
+        amount = 0.01 ether;
         require(Voted[msg.sender] == false); // Voters should not cast their votes before
         require(msg.value >= 0.01 ether, "SMALL_ETH");
         candidates[candidateId].totalVotes++;
-        
         Voted[msg.sender] = true;
+        prizeAmount.transfer(amount);
         
         // emit event
         emit UpVote(candidates[candidateId].name, candidates[candidateId].totalVotes);
@@ -59,12 +57,12 @@ contract Vote {
     function winnerVote(uint winnerVoteId_) public onlyOwner {
         winnerVoteId_ = Candidate[finishV0te()].name;
         uint prizeForCandidate = prizeAmount/100*90;
-        //payable(Candidate[finishV0te()]).transfer(prizeForCandidate);
+        Candidate[finishV0te()].transfer(prizeForCandidate);
     }
 
     function withdrawComissions() public onlyOwner {
         uint comissions = prizeAmount/100*10;
-        //payable(owner.transfer(comissions));
+        owner.transfer(comissions);
     }
     
     function showCandidate() public view returns(address[] memory) {
